fcdb47c70169bfba2b8b6fc3d7b7bf87e0a8da6a
Gabriella Piacentino <piacentg@bc.edu>
2023-04-28 16:41:55 -0400

    Initial Commit

---
+++ NovawaveManagementApp/NovawaveManagementAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NovawaveManagementAppApp.swift
+//  NovawaveManagementApp
+//
+//  Created by Gabriella Piacentino on 4/28/23.
+//
+
+import SwiftUI
+
+@main
+struct NovawaveManagementAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
